5e7fb8217f988f6b1e7b585c29b90191b6a9de29
Sam Kuhbander <kuhbansc@miamioh.edu>
2021-04-08 16:25:07 -0400

    testing database

--- login.inc.php
+++ login.inc.php
@@ -1,29 +1,13 @@
 <?php
-
-	//Get values from signIn.php
-	
-	$username = $_POST['user'];
-	$password = $_POST['pass'];
-
-	//SQL Injection
-	$username = stripslashes($username);
-	$password = stripslashes($password);
-	$username = mysql_real_escape_string($username);
-	$password = mysql_real_escape_string($password);
-
-	echo $username;
-
-	//connect to the server
-	mysql_connect('localhost', 'root', '');
-	mysql_select_db("users");
-
-	//Query database 
-	$result = mysql_query("SELECT * FROM users WHERE usersName = '$username' AND usersPassword = '$password'") or die("Failed to reach database " . mysql_error());
-	$row = mysql_fetch_array($result);
-	if ($row['userName'] == $username && $row['usersPassword'] == $password){
-		echo "Login success! Welcome " . $row['userName'];
-	}	else {
-		echo "Failed login";
-	}
-
+$user="username";
+$password="password";
+$con=mysqli_connect("localhost",$user,$password, "database1");
+if(!$con)
+{
+echo "could not connect" ;
+}
+else
+{
+echo "connected";
+}
 ?>
