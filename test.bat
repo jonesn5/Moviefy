9094cb5895fce17ff7505d03a9cdf83fbe214ba8
jonesn5 <64740967+jonesn5@users.noreply.github.com>
2021-04-13 19:11:12 -0400

    Add files via upload

---
+++ test.bat
@@ -0,0 +1,8 @@
+
+echo NicksBatchFile
+mkdir testingfile1
+cd testingfile1
+curl -o index.php https://raw.githubusercontent.com/jonesn5/Moviefy/main/index.php
+echo done
+start localhost/index.php
+
