061e518859178ca002b2a7b8579e1d00d2c9d69a
sherveenm <79105831+sherveenm@users.noreply.github.com>
2021-04-15 15:26:14 -0400

    Add files via upload
    Updated build script

--- test.bat
+++ test.bat
@@ -1,15 +1,22 @@
 
 echo NicksBatchFile
-mkdir testingfile1
-cd testingfile1
-curl -o index.php https://raw.githubusercontent.com/jonesn5/Moviefy/main/index.php
-curl -o header.php https://raw.githubusercontent.com/jonesn5/Moviefy/main/header.php
-curl -o login.php https://raw.githubusercontent.com/jonesn5/Moviefy/main/login.php
-curl -o style.css https://raw.githubusercontent.com/jonesn5/Moviefy/main/style.css
-curl -o about.php https://raw.githubusercontent.com/jonesn5/Moviefy/main/about.php
-curl -o register.php https://raw.githubusercontent.com/jonesn5/Moviefy/main/register.php
-curl -o search.php https://raw.githubusercontent.com/jonesn5/Moviefy/main/search.php
-curl -o server.php https://raw.githubusercontent.com/jonesn5/Moviefy/main/server.php
+
+mkdir 	XAMPP
+mkdir 	testingfile1
+
+start "" https://www.apachefriends.org/xampp-files/8.0.3/xampp-windows-x64-8.0.3-0-VS16-installer.exe
+
+cd 	testingfile1
+
+curl 	-o 	index.php 					https://raw.githubusercontent.com/jonesn5/Moviefy/main/index.php
+curl 	-o 	header.php 					https://raw.githubusercontent.com/jonesn5/Moviefy/main/header.php
+curl 	-o 	login.php 					https://raw.githubusercontent.com/jonesn5/Moviefy/main/login.php
+curl 	-o 	style.css 					https://raw.githubusercontent.com/jonesn5/Moviefy/main/style.css
+curl 	-o 	about.php 					https://raw.githubusercontent.com/jonesn5/Moviefy/main/about.php
+curl 	-o 	register.php 					https://raw.githubusercontent.com/jonesn5/Moviefy/main/register.php
+curl 	-o 	search.php 					https://raw.githubusercontent.com/jonesn5/Moviefy/main/search.php
+curl 	-o 	server.php 					https://raw.githubusercontent.com/jonesn5/Moviefy/main/server.php
+
 echo done
 
 
