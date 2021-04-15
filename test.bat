46bb613d6293f7ae413852d6a545ddce4b93e6a3
jonesn5 <64740967+jonesn5@users.noreply.github.com>
2021-04-15 10:52:35 -0400

    Update test.bat

--- test.bat
+++ test.bat
@@ -3,6 +3,13 @@ echo NicksBatchFile
 mkdir testingfile1
 cd testingfile1
 curl -o index.php https://raw.githubusercontent.com/jonesn5/Moviefy/main/index.php
+curl -o header.php https://raw.githubusercontent.com/jonesn5/Moviefy/main/header.php
+curl -o login.php https://raw.githubusercontent.com/jonesn5/Moviefy/main/login.php
+curl -o style.css https://raw.githubusercontent.com/jonesn5/Moviefy/main/style.css
+curl -o about.php https://raw.githubusercontent.com/jonesn5/Moviefy/main/about.php
+curl -o register.php https://raw.githubusercontent.com/jonesn5/Moviefy/main/register.php
+curl -o search.php https://raw.githubusercontent.com/jonesn5/Moviefy/main/search.php
+curl -o server.php https://raw.githubusercontent.com/jonesn5/Moviefy/main/server.php
 echo done
-start localhost/index.php
+
 
