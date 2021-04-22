cd34ed0a37e9dd25eb8200063b05a71fafff8c5c
jonesn5 <64740967+jonesn5@users.noreply.github.com>
2021-04-21 23:19:31 -0400

    Update test.bat

--- test.bat
+++ test.bat
@@ -7,19 +7,28 @@ start 	"" 	https://www.apachefriends.org/xampp-files/8.0.3/xampp-windows-x64-8.0
 
 cd 	Moviefy
 
-curl  -o  README.md  https://raw.githubusercontent.com/jonesn5/Moviefy/main/README.md
+curl	-o	README.md	https://raw.githubusercontent.com/jonesn5/Moviefy/main/README.md
 curl 	-o 	index.php 	https://raw.githubusercontent.com/jonesn5/Moviefy/main/index.php
 curl 	-o 	header.php 	https://raw.githubusercontent.com/jonesn5/Moviefy/main/header.php
-curl 	-o 	login.php 	https://raw.githubusercontent.com/jonesn5/Moviefy/main/login.php
 curl 	-o 	style.css 	https://raw.githubusercontent.com/jonesn5/Moviefy/main/style.css
 curl 	-o 	about.php 	https://raw.githubusercontent.com/jonesn5/Moviefy/main/about.php
+curl    -o	genre.php	https://raw.githubusercontent.com/jonesn5/Moviefy/main/genre.php
+curl	-o	database1.sql	https://raw.githubusercontent.com/jonesn5/Moviefy/Build_Script/database1.sql
+
+mkdir	Functions
+cd	Functions
+
+curl 	-o 	server.php 	https://raw.githubusercontent.com/jonesn5/Moviefy/main/server.php
+curl	-o	errors.php	https://raw.githubusercontent.com/jonesn5/Moviefy/main/Functions/errors.php
 curl 	-o 	register.php 	https://raw.githubusercontent.com/jonesn5/Moviefy/main/register.php
 curl 	-o 	search.php 	https://raw.githubusercontent.com/jonesn5/Moviefy/main/search.php
-curl 	-o 	server.php 	https://raw.githubusercontent.com/jonesn5/Moviefy/main/server.php
-curl  -o  genre.php  https://raw.githubusercontent.com/jonesn5/Moviefy/main/genre.php
+curl 	-o 	login.php 	https://raw.githubusercontent.com/jonesn5/Moviefy/main/login.php
+
+cd	..
 
 start README.md
 
 echo done
+
 
 
