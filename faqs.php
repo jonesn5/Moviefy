0454c71bcd888589e40cda762cfce7cc242e5d79
karimsammouri <64442487+karimsammouri@users.noreply.github.com>
2021-04-23 01:04:50 -0400

    Created FAQs page

---
+++ faqs.php
@@ -0,0 +1,120 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+  <title>Moviefy</title>
+  <!-- Website favicon -->
+  <link rel="shortcut icon" href="Images/moviefyIcon.png"> <!-- Go to root folder before finding img -->
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+    crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+    crossorigin="anonymous"></script>
+
+  <script>
+    //Dark mode 
+    $(document).ready(function () {
+      $("#buttonToggleColors").click(function () {
+        $('.navbar').toggleClass("navbar-light navbar-dark");
+        $('.navbar').toggleClass("bg-light bg-dark");
+      });
+    });
+  </script>
+
+    <!-- Internal CSS for FAQs-page text. -->
+    <style>
+    h1 {text-align: center;
+        margin: 1em;
+    }
+    h5 {text-align: center;
+    margin: 1em;
+    }
+    p {margin: 0em, 0em, 0em, 3em;
+    }
+    .FAQs {margin: 25px 50px;
+    }
+    </style>
+
+</head>
+
+<body>
+
+
+<!-- Boostrap navigation bar -->
+<nav class="navbar navbar-expand-md bg-light navbar-light">
+  <!-- Brand -->
+  <a class="navbar-brand" href="index.php">Moviefy</a>
+
+  <!-- Toggler/collapsibe Button -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <!-- Navbar links -->
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <!-- margin keeps login and search on right -->
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="genre.php">Genre</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="faqs.php">FAQs</a>
+      </li>
+      <li class="nav-item">
+        <!-- Search bar -->
+        <form class="form-inline" method="post" action="../Functions/search.php">
+          <input class="form-control " type="text" name="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-primary" type="submit" >Search</button>
+        </form>
+      </li>
+    </ul>
+
+    <!-- Toggle dark mode -->
+    <button id="buttonToggleColors" class="btn btn-primary">Dark Mode</button>
+    
+    <!-- sign in button -->
+    <a href="../index.php?logout='1'"> <button class="btn btn-outline-success" type="submit">Logout</button> </a>
+    <!-- logged in user information -->
+    <?php  if (isset($_SESSION['username'])) : ?>
+      <p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
+    <?php endif ?>
+  </div>
+</nav>
+
+    <div>
+    <h1> FAQs </h1>
+    <div class = "FAQs">
+    <h5> What is Moviefy? </h5>
+    <p> Moviefy is the premier one-stop online movie catalog in the web. With Moviefy's vast catalog of user-rated movies, it's easier than ever to decide which movie or show to binge-watch next! What are you waiting for? </p>
+    <h5> Is Moviefy Free? </h5>
+    <p> Thanks to our sponsors, Moviefy is completely free and we intend it to stay that way for the forseeable future. Those advertisements are not so bad after all, huh? </p>
+    <h5> Why was my comment taken down? </h5>
+    <p> A comment will be taken down if it violates our community policy. For more information on our community policy, feel free to contact our support. </p>
+    <h5> How do I contact support? </h5>
+    <p> You can reach our customer service at support@moviefy.com. We are more than happy to help you! </p>
+    </div>
+    </div>
+
+</body>
+
+</html>
