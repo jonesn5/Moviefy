be248bd925f4cc5f6e8c40959f213a8e28d94ea8
sherveenm <79105831+sherveenm@users.noreply.github.com>
2021-05-05 17:43:41 -0500

    Add files via upload

--- test.bat
+++ test.bat
@@ -12,16 +12,25 @@ curl 	-o 	header.php 	https://raw.githubusercontent.com/jonesn5/Moviefy/main/hea
 curl 	-o 	style.css 	https://raw.githubusercontent.com/jonesn5/Moviefy/main/style.css
 curl 	-o 	about.php 	https://raw.githubusercontent.com/jonesn5/Moviefy/main/about.php
 curl    -o	genre.php	https://raw.githubusercontent.com/jonesn5/Moviefy/main/genre.php
-curl	-o	database1.sql	https://raw.githubusercontent.com/jonesn5/Moviefy/Build_Script/database1.sql
+curl	-o	movies.sql	https://raw.githubusercontent.com/jonesn5/Moviefy/main/movies.sql
+curl 	-o 	login.php 	https://raw.githubusercontent.com/jonesn5/Moviefy/main/login.php
+curl    -o	admin.php	https://raw.githubusercontent.com/jonesn5/Moviefy/main/admin.php
+curl 	-o 	delete.php	https://raw.githubusercontent.com/jonesn5/Moviefy/main/delete.php
+curl 	-o 	search.php 	https://raw.githubusercontent.com/jonesn5/Moviefy/main/search.php
+curl 	-o 	server.php 	https://raw.githubusercontent.com/jonesn5/Moviefy/main/server.php
 
 mkdir	Functions
 cd	Functions
 
-curl 	-o 	server.php 	https://raw.githubusercontent.com/jonesn5/Moviefy/main/server.php
+curl 	-o 	server.php 	https://raw.githubusercontent.com/jonesn5/Moviefy/main/Functions/server.php
 curl	-o	errors.php	https://raw.githubusercontent.com/jonesn5/Moviefy/main/Functions/errors.php
-curl 	-o 	register.php 	https://raw.githubusercontent.com/jonesn5/Moviefy/main/register.php
-curl 	-o 	search.php 	https://raw.githubusercontent.com/jonesn5/Moviefy/main/search.php
-curl 	-o 	login.php 	https://raw.githubusercontent.com/jonesn5/Moviefy/main/login.php
+curl 	-o 	register.php 	https://raw.githubusercontent.com/jonesn5/Moviefy/main/Functions/register.php
+curl 	-o 	search.php 	https://raw.githubusercontent.com/jonesn5/Moviefy/main/Functions/search.php
+curl 	-o 	suggest.php 	https://raw.githubusercontent.com/jonesn5/Moviefy/main/Functions/suggest.php
+curl 	-o 	login.php 	https://raw.githubusercontent.com/jonesn5/Moviefy/main/Functions/login.php
+
+
+
 
 cd	..
 
