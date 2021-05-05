f1ab258f179bd16df8088c81940a1aae7f302881
samkuhbander <55243087+samkuhbander@users.noreply.github.com>
2021-05-05 10:00:43 -0400

    Update movies.sql

--- movies.sql
+++ movies.sql
@@ -2,8 +2,8 @@
 -- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 04, 2021 at 04:25 PM
+-- Host: localhost
+-- Generation Time: May 04, 2021 at 04:44 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
@@ -24,6 +24,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `comment_user` varchar(40) NOT NULL,
+  `pageID` varchar(40) NOT NULL,
+  `comment` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`comment_user`, `pageID`, `comment`) VALUES
+('sa', 'us-2019', 'Looks scary!'),
+('sa', 'blackwidow-2020', 'Excited for this');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `movies`
 --
 
@@ -32,21 +52,130 @@ CREATE TABLE `movies` (
   `director` varchar(100) NOT NULL,
   `genre` varchar(100) NOT NULL,
   `pageID` varchar(100) NOT NULL,
-  `Runtime` int(11) NOT NULL,
-  `description` text NOT NULL,
-  `image` varchar(100) NOT NULL,
-  `year` int(11) NOT NULL,
-  `rating` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `Runtime` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `movies`
 --
 
-INSERT INTO `movies` (`movie`, `director`, `genre`, `pageID`, `Runtime`, `description`, `image`, `year`, `rating`) VALUES
-('Black Widow', 'Cate Shortland', 'Action', 'blackwidow-2020', 133, '', '', 0, 0),
-('Us', 'Jordan Peele', 'Horror', 'us-2019', 121, 'Us is a 2019 American horror film written and directed by Jordan Peele, starring Lupita Nyong\'o, Winston Duke, Elisabeth Moss, and Tim Heidecker. The film follows Adelaide Wilson (Nyong\'o) and her family, who are attacked by a group of menacing doppelgängers. The project was announced in February 2018, and much of the cast joined in the following months. Peele produced the film alongside Jason Blum and Sean McKittrick (the trio previously having collaborated on Get Out and BlacKkKlansman), as well as Ian Cooper. Filming took place from July to October 2018 in California, mostly in Los Angeles, Pasadena and Santa Cruz. (Horror)', 'Us_Poster.png', 2019, 4),
-('A Quiet Place', 'John Krasinski', 'Horror', 'aquietplace-2018', 90, 'A Quiet Place is a 2018 American horror film directed by John Krasinski and written by Bryan Woods, Scott Beck and Krasinski. The plot revolves around a father (Krasinski) and a mother (Emily Blunt) who struggle to survive and raise their children (Millicent Simmonds and Noah Jupe) in a post-apocalyptic world inhabited by blind monsters with an acute sense of hearing.', 'AQuietPlace_Poster.png', 2018, 4);
+INSERT INTO `movies` (`movie`, `director`, `genre`, `pageID`, `Runtime`) VALUES
+('Black Widow', 'Cate Shortland', 'Action', 'blackwidow-2020', 133),
+('Us', 'Jordan Peele', 'Horror', 'us-2019', 121),
+('Dolittle', 'Stephen Gaghan', 'Family', 'dolittle-2020', 101);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ratings`
+--
+
+CREATE TABLE `ratings` (
+  `rating` int(10) DEFAULT NULL,
+  `pageID` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `ratings`
+--
+
+INSERT INTO `ratings` (`rating`, `pageID`) VALUES
+(5, 'us-2019'),
+(4, 'us-2019'),
+(4, 'us-2019'),
+(4, 'us-2019'),
+(4, 'us-2019'),
+(3, 'us-2019'),
+(4, 'us-2019'),
+(3, 'us-2019'),
+(4, 'us-2019'),
+(5, 'us-2019'),
+(4, 'us-2019'),
+(4, 'us-2019'),
+(2, 'us-2019'),
+(5, 'us-2019'),
+(5, 'dolittle-2020'),
+(4, 'dolittle-2020'),
+(3, 'dolittle-2020'),
+(5, 'us-2019'),
+(5, 'us-2019'),
+(5, 'us-2019'),
+(4, 'blackwidow-2020'),
+(5, 'blackwidow-2020'),
+(4, 'dolittle-2020'),
+(4, 'dolittle-2020'),
+(5, 'dolittle-2020'),
+(5, 'blackwidow-2020');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `suggestions`
+--
+
+CREATE TABLE `suggestions` (
+  `suggest` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `suggestions`
+--
+
+INSERT INTO `suggestions` (`suggest`) VALUES
+(''),
+(''),
+('Happy Gilmore'),
+('Nemo'),
+('Up'),
+('Up'),
+('Up'),
+(''),
+(''),
+('Godzilla'),
+('Godzilla'),
+('Spy Kids');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `Admin` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `Admin`) VALUES
+(1, 'sa', 'sa@gmail.com', '202cb962ac59075b964b07152d234b70', 1),
+(5, 'test', 't@gmail.com', '202cb962ac59075b964b07152d234b70', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
